07189c22584237e4d9045abc707286166d3db868
HoaHA <HoaHA@brainservice.onmicrosoft.com>
2026-08-19 10:48:56 +0900

    Upgrade

--- wwwroot/js/pcm-worklet.js
+++ wwwroot/js/pcm-worklet.js
@@ -1,52 +1,30 @@
 ﻿class PcmWorkletProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
-
         this.targetSampleRate = 16000;
-
-        // Gửi khoảng mỗi 100ms.
-        this.targetBufferSize = 1600;
-
+        this.targetBufferSize = 8000; // 500ms
         this.pcmBuffer = new Int16Array(this.targetBufferSize);
         this.bufferOffset = 0;
     }
 
     process(inputs) {
         const input = inputs[0];
-
-        if (!input || !input[0]) {
-            return true;
-        }
+        if (!input || !input[0]) return true;
 
         const channelData = input[0];
-
         const ratio = sampleRate / this.targetSampleRate;
-        const outputLength =
-            Math.floor(channelData.length / ratio);
+        const outputLength = Math.floor(channelData.length / ratio);
 
         for (let i = 0; i < outputLength; i++) {
             const sourceIndex = Math.floor(i * ratio);
-
-            let sample = channelData[sourceIndex];
-
-            sample = Math.max(-1, Math.min(1, sample));
-
-            const pcm16 =
-                sample < 0
-                    ? sample * 0x8000
-                    : sample * 0x7fff;
+            let sample = Math.max(-1, Math.min(1, channelData[sourceIndex]));
+            const pcm16 = sample < 0 ? sample * 0x8000 : sample * 0x7fff;
 
             this.pcmBuffer[this.bufferOffset++] = pcm16;
 
             if (this.bufferOffset >= this.targetBufferSize) {
-                const output =
-                    this.pcmBuffer.slice(0);
-
-                this.port.postMessage(
-                    output.buffer,
-                    [output.buffer]
-                );
-
+                const output = this.pcmBuffer.slice();
+                this.port.postMessage(output.buffer, [output.buffer]);
                 this.bufferOffset = 0;
             }
         }
@@ -55,7 +33,4 @@
     }
 }
 
-registerProcessor(
-    "pcm-worklet-processor",
-    PcmWorkletProcessor
-);
+registerProcessor("pcm-worklet-processor", PcmWorkletProcessor);
